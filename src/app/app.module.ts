73892f93b812116020960b6a5125da357079d01e
tusharh8888 <tusharhotani998@gmail.com>
2025-04-05 16:07:21 +0530

    Updated component with new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // ✅ Import HttpClientModule
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { GridComponent } from './component/grid/grid.component';
@@ -11,7 +12,8 @@ import { GridComponent } from './component/grid/grid.component';
   ],
   imports: [
     BrowserModule,
-    AgGridModule // Import AgGridModule
+    AgGridModule,
+    HttpClientModule // ✅ Add HttpClientModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
